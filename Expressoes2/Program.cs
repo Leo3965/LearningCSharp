93f9211dab26294385db9a4e0cde7f88e33a813a
Léo Freitas <leonardo.eng3965@gmail.com>
2021-03-04 21:56:22 -0300

    OrderBy and Icomparable

---
+++ Expressoes2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Expressoes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //string padrao = "[0-9]{4}[-][0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+
+            //string textoDeTeste = "Meu nome é Guilherme, me ligue em 4784-4546";
+            string textoDeTeste = "Meu nome é Guilherme, me ligue em 94784-4546";
+
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+
+            Console.WriteLine(resultado.Value);
+            Console.ReadLine();
+        }
+    }
+}
